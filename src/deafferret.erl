59647d6e64d18d84c304f0622c3693a069fd4fea
Hans Ulrich Niedermann <hun@n-dimensional.de>
2009-07-03 21:45:45 +0200

    Rename "Dict" to "Alphabet"

--- src/deafferret.erl
+++ src/deafferret.erl
@@ -4,24 +4,24 @@
 -export([mutate/1, mutate/2]).
 
 
-%% BUG: If the OrigSequence contains elements not in Dict -> wrong result.
+%% BUG: If the OrigSequence contains elements not in Alphabet -> wrong result.
 %% FIXME: We are using ++ in two places, which mostly is not a good idea.
 %% NOTE: We return the results in a very strange kind of "ordering".
 %% NOTE: A simple test case of "AAAA" lets you miss permutated characters.
 
 
 %% Verbose version. Comment out the io:format stuff for a mute version.
-mutate([], _Dict, _Prefix, Acc) ->
+mutate([], _Alphabet, _Prefix, Acc) ->
     lists:append(Acc);
-mutate([Base|Suffix]=_OrigSequence, Dict, Prefix, Acc) ->
-    io:format("mutate(~p,~p,~p,~p)~n", [_OrigSequence,Dict,Prefix,Acc]),
-    NewMutations = [Prefix++[X|Suffix] || X<-Dict, X=/=Base],
+mutate([Base|Suffix]=_OrigSequence, Alphabet, Prefix, Acc) ->
+    io:format("mutate(~p,~p,~p,~p)~n", [_OrigSequence,Alphabet,Prefix,Acc]),
+    NewMutations = [Prefix++[X|Suffix] || X<-Alphabet, X=/=Base],
     io:format("  new: ~p~n", [NewMutations]),
-    mutate(Suffix, Dict, Prefix++[Base], [NewMutations|Acc]).
+    mutate(Suffix, Alphabet, Prefix++[Base], [NewMutations|Acc]).
 
 
-mutate(OrigSequence, Dict) ->
-    mutate(OrigSequence, Dict, "", []).
+mutate(OrigSequence, Alphabet) ->
+    mutate(OrigSequence, Alphabet, "", []).
 
 mutate(OrigSequence) ->
     mutate(OrigSequence, "ACGT").
@@ -29,11 +29,10 @@ mutate(OrigSequence) ->
 
 %% Run example.
 example(OrigSequence) ->
-    Dict = "ACGT",
-    Mutations = mutate(OrigSequence, Dict),
+    Mutations = mutate(OrigSequence),
     io:format("Mutations of orig sequence ~p for dictionary ~p:~n"
 	      "  ~p~n",
-	      [OrigSequence, Dict,
+	      [OrigSequence, Alphabet,
 	       Mutations]).
 
 start() ->
